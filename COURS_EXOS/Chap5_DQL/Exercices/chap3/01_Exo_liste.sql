732f87a92c420cc83744d44d178b0cf2b2b17b4b
Antoine07 <antoine.lucsko@gmail.com>
2023-02-09 13:49:05 +0100

    chap

--- COURS_EXOS/Chap5_DQL/Exercices/chap3/01_Exo_liste.sql
+++ COURS_EXOS/Chap5_DQL/Exercices/chap3/01_Exo_liste.sql
@@ -33,6 +33,73 @@ FROM pilots
 WHERE num_flying >= ALL(SELECT num_flying FROM pilots);
 
 
+
 SELECT name, num_flying
 FROM pilots
 WHERE num_flying < ANY(SELECT num_flying FROM pilots);
+
+
+--- 03 
+/*
+Faites la somme des heures de vol des pilotes dont le nom de la compagnie est Air France. Bien sûr, vous ne connaissez pas la clef primaire de la compagnie.
+*/
+
+SELECT SUM(num_flying)
+FROM pilots
+WHERE `certificate` IN (
+    SELECT `certificate`
+    FROM pilots
+    WHERE company IN (
+        SELECT comp 
+        FROM companies
+        WHERE `name`= 'Air France'
+    )
+);
+
+SELECT SUM(num_flying) as sum_flying
+FROM pilots
+WHERE `company` = (SELECT comp FROM companies WHERE `name`='Air France');
+
+
+-- 
+/*
+comp = ITA, street = mapoli, city = Rome, name = Italia Air, num_street = 20
+*/
+
+INSERT INTO companies 
+SET comp = 'ITA', street = 'mapoli', city = 'Rome', `name` = 'Italia Air', num_street = 20 ;
+
+/*
+Trouvez toutes les compagnies n'ayant pas de pilotes.
+*/
+
+SELECT `name`, comp 
+FROM companies
+WHERE comp NOT IN (SELECT company FROM pilots);
+
+SELECT name, num_street, street, city
+FROM companies
+WHERE comp IN(
+    SELECT company
+    FROM pilots
+    WHERE num_flying < ALL(
+        SELECT num_flying
+        FROM pilots
+        WHERE plane='A380'
+    )
+);
+
+ALTER TABLE `pilots`
+ADD `last_name` VARCHAR(100) AFTER `name` ;
+
+UPDATE `pilots`
+SET `last_name` = (
+    CASE
+        WHEN `name` IN ('alan', 'yi', 'sophie') THEN 'Dupont'
+        WHEN `name` IN ('tom') THEN 'Lu'
+        WHEN `name` IN ('Albert') THEN 'Pierre'
+        ELSE 'chai'
+    END
+);
+
+-- requête corrélée
